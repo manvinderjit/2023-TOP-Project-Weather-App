ba9358e66db95766413a5c26e6b1ed1ba6d33f6c
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-09-09 20:54:54 -0400

    feat: Add data display

--- src/modules/home.js
+++ src/modules/home.js
@@ -4,6 +4,8 @@ const homeContent = () => {
     createMainElement();
     createWeatherForm();
     submitFormBehavior();
+    createWeatherDisplay();
+    populateWeatherDisplay();
 };
 
 const createMainElement = () => {
@@ -36,14 +38,74 @@ const submitFormBehavior = () => {
     const formElement = document.getElementById('get-weather-data');
     formElement.addEventListener('submit', (event) => {
         const location = document.getElementById('location').value;
-        const data = fetchWeatherData(location);
-        console.log(data);
+        populateWeatherDisplay(location);
         event.preventDefault();
     });
 };
 
-const createWeatherDisplaySection = () => {
+const createWeatherDisplay = () => {
+    const mainElement = document.querySelector('main');
     const displayWrapper = document.createElement('div');
+    displayWrapper.id = 'display-wrapper';
+
+    const displayHeader = createWeatherDisplayHeader();
+    displayWrapper.appendChild(displayHeader);
+
+    const displaySubheader = createWeatherDisplaySubheader();
+    displayWrapper.appendChild(displaySubheader);
+    mainElement.appendChild(displayWrapper);
+};
+
+const createWeatherDisplayHeader = () => {
+    const displayHeader = document.createElement('div');
+    displayHeader.classList.add('display-header');
+
+    const displayLocation = document.createElement('h1');
+    displayLocation.id = 'current-location';
+    displayHeader.appendChild(displayLocation);
+
+    const displayCurrentTemperature = document.createElement('div');
+    displayCurrentTemperature.id = 'current-temperature';
+    displayHeader.appendChild(displayCurrentTemperature);
+
+    // const toggleTemperature = document.createElement('div');
+    // toggleTemperature.id = 'toggle-temperature';
+    // toggleTemperature.textContent = 'To F';
+    // displayHeader.appendChild(toggleTemperature);
+
+    return displayHeader;
+};
+
+const createWeatherDisplaySubheader = () => {
+    const displaySubheader = document.createElement('div');
+    displaySubheader.id = 'display-subheader';
+
+    const displayRegion = document.createElement('p');
+    displayRegion.id = 'display-region';
+    displaySubheader.appendChild(displayRegion);
+
+    const displayFeelsLike = document.createElement('p');
+    displayFeelsLike.id = 'feels-like';
+    displaySubheader.appendChild(displayFeelsLike);
+
+    return displaySubheader;
+};
+
+const populateWeatherDisplay = async (location) => {
+    const data = await fetchWeatherData(location);
+    console.log(data);
+    const locationName = document.getElementById('current-location');
+    locationName.textContent = data.name;
+    const locationTemp = document.getElementById('current-temperature');
+    locationTemp.textContent = data.temp_c;
+    const displayRegion = document.getElementById('display-region');
+    displayRegion.textContent = `${data.region}, ${data.country}`;
+    const displayFeelsLike = document.getElementById('feels-like');
+    displayFeelsLike.textContent = `Feels Like: ${data.feelslike_c}`;
+};
+
+const switchTemperatureUnit = () => {
+
 };
 
 export { homeContent };
